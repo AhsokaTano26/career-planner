3eb1a277ba4a61b7803d16d5fb3a9a13220f6a2b
Tano <github@tano.asia>
2026-08-17 22:46:49 +0100

    feat: add integrated role-based frontend

---
+++ fronted/src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './styles.css'
+import './functional.css'
+import './lists.css'
+import './logout.css'
+
+createApp(App).mount('#app')
